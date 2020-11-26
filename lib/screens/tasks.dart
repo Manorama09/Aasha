98ed838519a9cbe2141679798f716faa78dbc8a3
Manorama09 <manoramamaharana@gmail.com>
2020-11-26 22:53:27 +0530

    Improved spacing on tasks oage

--- lib/screens/tasks.dart
+++ lib/screens/tasks.dart
@@ -16,7 +16,7 @@ class _TasksScreenState extends State<TasksScreen> {
       margin: EdgeInsets.symmetric(horizontal: 2, vertical: 1),
       child: Card(
           elevation: 4.0,
-          margin: new EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
+          margin: new EdgeInsets.symmetric(horizontal: 0.0, vertical: 4.0),
           child: Container(
               decoration: BoxDecoration(
                   color: const Color(0xffB180FE).withOpacity(0.45)),
